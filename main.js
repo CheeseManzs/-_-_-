d003ee0a674a122a3399fbb27b11e876df54d233
CheeseMans <36460677+CheeseManzs@users.noreply.github.com>
2021-11-05 17:53:09 -0400

    setup main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+const { Console } = require('console');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+//prefix that people use
+const pre = 'shr!'
+//just letting me know when it goes live...
+client.once('ready', () => {console.log("Shrugbot is live!")})
+
+
+
+
+
+client.on('message', message => {
+
+
+
+});
+
+
+
+
+
+
+//THIS GOES LAST
+client.login('OTA2MjkxMTkyNzI2MTg4MDYy.YYWfcg.ul1HJrQii90DV7X8Al5FlzvUwqc');
