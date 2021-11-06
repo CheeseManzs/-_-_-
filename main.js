fe4bb87238d3d5b32180f6c3451f01251f040a36
CheeseMans <36460677+CheeseManzs@users.noreply.github.com>
2021-11-06 17:42:16 -0400

    finished reputation system (for now)

--- main.js
+++ main.js
@@ -18,30 +18,85 @@ client.once('ready', () => {
 //dictionary of commands
 var cmdDict = new Map();
 cmdDict.set('hi', funclib1.test);
+cmdDict.set('rep', getreputation)
 
-client.on('messageCreate', (message) => {
+function resolveAfterTSeconds(t) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(10);
+      }, t*1000);
+    });
+  }
+
+
+  //reputation should ALWAYS be displayed as the rounded version of rep*100
+async function getreputation(message, args)
+{   
+    try{
+        if(client.guilds.cache.get(message.guildId).members.cache.get(args[0]) != undefined){
+            console.log("args[0]: " + args[0])
+            var target = args[0].replace("<","").replace(">","").replace("@","").replace("!","")
+            var repvalue = await rep.get_rep(message.guildId, target);
+            var repvalue = Math.round(repvalue*100)
+            var targetname = client.users.cache.find(user => user.id === args[0]).username;
+            message.channel.send(targetname+": "+repvalue);
+        }else
+        {
+            message.channel.send("That person is not in this server!")
+        }
+    }
+    catch(err)
+    {
+        message.channel.send(err.message);
+    }
+}
+
+
+client.on('messageCreate', async(message) => {
 
     //reputation monitor
     if(message.author.bot){ return;}
-    console.log("modifying reputation!")
-    rep.modify_user(message.guildId, message.author.id, 0.01)
-    rep.get_rep(message.guildId, message.author.id)
-    message.channel.send("Updating Reputation");
+    if(message.content.replace(/ /g, "").length >= 4)
+    {
+        var offenses = await rep.get_off(message.guildId, message.author.id);
+        //console.log(offenses)
+        //console.log(offenses+1)
+        await rep.modify_user(message.guildId, message.author.id, 0.005/((offenses+1)/5));
+        //console.log("modified reputation for '" + message.author.username+"'");
+        //console.log(await rep.get_rep(message.guildId, message.author.id))
+        //line below is an example on how to get reputation
+        //var repval = await rep.get_rep(message.guildId, message.author.id);
+    }
+
     //command detection
     if(!message.content.startsWith(pre)){ return;}
+
     //get arguments
     var args = []
-    toadd = message.content.slice(pre.length).split(" ")
+    toadd = message.content.slice(pre.length).split(/ /g)
+
     //add in the arguments
     toadd.forEach(element => {
-        console.log(element)
         args.push(element.toLowerCase());
     });
-    console.log(args)
-    const command = args;
-    console.log(cmdDict.get(command[0]))
-    if(cmdDict.get(command[0]) != undefined){
-        cmdDict.get(command[0])(message);
+    //get the command argument
+    const command = args[0];
+    for (let index = 0; index < args.length; index++) {
+        if(args[index].startsWith("<@!")){
+            args[index] = args[index].substring(3, args[index].length-1)
+        }       
+    }
+    //check if it exists and then run the command
+    if(cmdDict.get(command) != undefined){
+        try
+        {
+            args.shift();
+            cmdDict.get(command)(message, args);
+        }
+        catch
+        {
+            message.channel.send("Error trying to evaluate command from request by " + message.author);
+        }
     }
 
 });
--- repsystem.js
+++ repsystem.js
@@ -21,7 +21,7 @@ async function modify_user(guildID, uID, amt)
     let db = new sql3.Database('rep.db');
     db.serialize(function() {
         //actual sqlite3 code
-        db.run("CREATE TABLE IF NOT EXISTS g"+guildID+"(uID int, value real, offenses int, UNIQUE(uID))", function(err){if(err){console.log(err.message)}});
+        db.run("CREATE TABLE IF NOT EXISTS g"+guildID+"(uID int, value real, offenses int, UNIQUE(uID))", function(err){if(err){console.log("1: " + err.message)}});
         try
         {
             db.run("INSERT INTO g"+guildID+" VALUES ("+uID+",0,0);",function(err){if(err){}});
@@ -30,7 +30,7 @@ async function modify_user(guildID, uID, amt)
             console.log("duplicate insertion");
         }
         var search = "(SELECT value FROM g"+guildID+" WHERE uID ="+uID+")"
-        db.run("UPDATE g"+guildID+" SET value = ("+search+"+"+amt+") WHERE uID="+uID,function(err){if(err){console.log(err.message)}})
+        db.run("UPDATE g"+guildID+" SET value = ("+search+"+"+amt+") WHERE uID="+uID,function(err){if(err){console.log("3: " + err.message)}})
         //db.each("SELECT uID, value, offenses FROM g"+guildID, function(err, row){
         //    
         //    if(err){
@@ -45,17 +45,38 @@ async function modify_user(guildID, uID, amt)
     //console.log("100% - closed!")
 }
 
-function get_rep(guildID, uID)
+async function get_rep(guildID, uID)
 {
     let db = new sql3.Database('rep.db');
-    db.serialize(function() {
-
-        db.each("SELECT uID, value FROM g"+guildID+" WHERE uID="+uID, function(err, row){
-
-            console.log("found: " + row.value);
-
-        });
-
-    })
+    val = null;
+    db.each("SELECT uID, value FROM g"+guildID+" WHERE uID="+uID, function(err, row){
+        if(err){
+            console.log(err.message);
+        }
+        val = row.value;
+    });
+    await resolveAfterTSeconds(0.25, 10);
+    return val;
 }
-module.exports = {modify_user, get_rep}
+
+async function get_off(guildID, uID)
+{
+    let db = new sql3.Database('rep.db');
+    val = null;
+    db.each("SELECT uID, value, offenses FROM g"+guildID+" WHERE uID="+uID, function(err, row){
+        val = row.offenses;
+    });
+    await resolveAfterTSeconds(0.25, 10);
+    return val;
+}
+
+function resolveAfterTSeconds(t,x) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, t*1000);
+    });
+  }
+
+
+module.exports = {modify_user, get_rep, get_off}
